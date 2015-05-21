cadebfe008b7cfcfe14523ba81c48e9cd1cd2f04
Jihoon Lee <jihoonlee.in@gmail.com>
2015-05-21 19:18:48 +0900

    add rappsg

---
+++ hello_rapps/configure_turtlebot.sh
@@ -0,0 +1,3 @@
+export TURTLEBOT_RAPP_PACKAGE_WHITELIST="[rocon_apps, turtlebot_rapps, hello_rapps]"
+export TURTLEBOT_INTERACTIONS_LIST="[turtlebot_bringup/admin.interactions, turtlebot_bringup/documentation.interactions, turtlebot_bringup/pairing.interactions, turtlebot_bringup/visualisation.interactions, hello_rapps/my.interactions]"
+
